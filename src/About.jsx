9530f56074420e7444c23a2a16395e93f419e427
Dounia-doun <klkoulrochelle123@gmail.com>
2026-03-15 14:58:37 +0100

    Fix: Correction image About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -86,7 +86,7 @@ export default function About() {
               aspectRatio: '4/3',
             }}>
               <img
-                src="/Public/images/hero7.jpg?t=1"
+                src="/public/images/hero1.jpg"
                 alt="Intérieur du restaurant Le Jasmin"
                 style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
               />
